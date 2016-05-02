2a76d3687585a4fe369bb825cc791a99f910c4cf
Jason Hills <jasonahills@gmail.com>
2016-05-02 17:57:11 -0600

    adds typescript transpiling and some boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # crew-station
 An OpenStardrive crew station.
+
+
+## Install
+
+- Install system dependencies
+    - NodeJS
+- Install the other stuffs
+    - `npm install`
+
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: [
+    './src/index.ts'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'main.js'
+  },
+  module: {
+    loaders: [
+      // {
+      //   test: /\.js$/,
+      //   loader: 'babel',
+      //   query: {
+      //     presets: ['es2015', 'stage-0', 'react']
+      //   }
+      // },
+      {
+        test: /\.tsx?/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  },
+  devServer: {
+    port: 3001
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: 'node_modules/html-webpack-template/index.ejs',
+      title: 'React',
+      devServer: 'http://localhost:3001',
+      appMountId: 'app'
+    })
+  ]
+}
